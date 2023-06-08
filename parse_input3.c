2336790860387ab4674214f01dc62b6994be2fd7
pcocci42 <paolo.cocci@arts.cornish.edu>
2023-06-09 00:44:29 +0200

    fix_quote

--- parse_input3.c
+++ parse_input3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_input3.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmasetti <lmasetti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paolococci <paolococci@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 15:33:41 by pcocci            #+#    #+#             */
-/*   Updated: 2023/06/08 16:57:58 by lmasetti         ###   ########.fr       */
+/*   Updated: 2023/06/09 00:43:22 by paolococci       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,16 +80,16 @@
 //}
 
 void	handle_dquote(t_cmd *cmd)
-{
-	if (cmd->cmd[cmd->ints->i - 1] != '=')
+{	
+	if (ft_chr(cmd->cmd, '"', cmd->ints->i + 1) == 0)
+		cmd->dquote = 1;
+	if (cmd->cmd[cmd->ints->i - 1] != '=' && cmd->dquote == 1)
 		cmd->cmd[cmd->ints->i] = ' ';
-	else
+	else if (cmd->dquote == 1)
 		cmd->cmd[cmd->ints->i] = 19;
 	cmd->ints->j = cmd->ints->i -1;
 	cmd->ints->i++;
 	cmd->squote = 0;
-	if (ft_chr(cmd->cmd, '"', cmd->ints->i) == 0)
-		cmd->dquote = 1;
 	while (cmd->cmd[cmd->ints->i] != '"' && cmd->dquote == 1)
 	{
 		if (cmd->cmd[cmd->ints->i] == ' ')
@@ -100,24 +100,24 @@ void	handle_dquote(t_cmd *cmd)
 			cmd->cmd[cmd->ints->i] = '$';
 		cmd->ints->i++;
 	}
-	if (cmd->cmd[cmd->ints->j] != '=')
+	if (cmd->cmd[cmd->ints->j] != '=' && cmd->dquote == 1)
 		cmd->cmd[cmd->ints->i] = ' ';
-	else
+	else if (cmd->dquote == 1)
 		cmd->cmd[cmd->ints->i] = 19;
 	cmd->dquote = 0;
 }
 
 void	handle_squote(t_cmd *cmd)
 {
-	if (cmd->cmd[cmd->ints->i - 1] != '=')
+	if (ft_chr(cmd->cmd, '\'', cmd->ints->i + 1) == 0)
+		cmd->squote = 1;
+	if (cmd->cmd[cmd->ints->i - 1] != '=' && cmd->squote == 1)
 		cmd->cmd[cmd->ints->i] = ' ';
-	else
+	else if (cmd->squote == 1)
 		cmd->cmd[cmd->ints->i] = 19;
 	cmd->ints->j = cmd->ints->i -1;
 	cmd->ints->i++;
 	cmd->dquote = 0;
-	if (ft_chr(cmd->cmd, '\'', cmd->ints->i) == 0)
-		cmd->squote = 1;
 	while (cmd->cmd[cmd->ints->i] != '\'' && cmd->squote == 1)
 	{
 		if (cmd->cmd[cmd->ints->i] == ' ')
@@ -128,9 +128,9 @@ void	handle_squote(t_cmd *cmd)
 			cmd->cmd[cmd->ints->i] = 20;
 		cmd->ints->i++;
 	}
-	if (cmd->cmd[cmd->ints->j] != '=')
+	if (cmd->cmd[cmd->ints->j] != '=' && cmd->squote == 1)
 		cmd->cmd[cmd->ints->i] = ' ';
-	else
+	else if (cmd->squote == 1)
 		cmd->cmd[cmd->ints->i] = 19;
 	cmd->squote = 0;
 }
